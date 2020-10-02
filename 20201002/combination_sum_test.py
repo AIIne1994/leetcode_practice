444ca2bf11755f6aa0f49eb598a95912f69c3ec5
Allen1994 <chengcl1994@gmail.com>
2020-10-02 22:50:18 +0800

    20201002 test_fix

--- 20201002/combination_sum_test.py
+++ 20201002/combination_sum_test.py
@@ -1,15 +1,15 @@
 import unittest
-from number_of_recent_calls import *
+from combination_sum import *
 
 class NumberOfRecentCallsTest(unittest.TestCase):
     def test_simple_one(self):
-        candidates =[2, 3, 6, 6]
+        candidates = [2, 3, 6, 6]
         target = 7
         expected = [[2, 2, 3], [7]]
         self.assertEqual(combination_sum(candidates, target), expected)
 
     def test_example_two(self):
-        candidates =[2, 3, 5]
+        candidates = [2, 3, 5]
         target = 8
         expected = [[2, 2, 2, 2], [2, 3, 3], [3, 5]]
         self.assertEqual(combination_sum(candidates, target), expected)
@@ -21,7 +21,7 @@ class NumberOfRecentCallsTest(unittest.TestCase):
         self.assertEqual(combination_sum(candidates, target), expected)
 
     def test_example_four(self):
-        candidates =[1]
+        candidates = [1]
         target = 1
         expected = [[1]]
         self.assertEqual(combination_sum(candidates, target), expected)
