71ffdba18512759da9108fb8df1023e6a2bff7af
Allen1994 <chengcl1994@gmail.com>
2020-10-04 23:58:43 +0800

    20201004 remove_coverd_interval solution

--- 20201004/remove_covered_intervals.py
+++ 20201004/remove_covered_intervals.py
@@ -1,7 +1,15 @@
 def remove_covered_intervals(intervals):
-    pass
+    intervals.sort(key=lambda interval: (interval[0], -interval[1]))
+    maximum = -1
+    removed = 0
+    for interval in intervals:
+        if interval[1] <= maximum:
+            removed += 1
+        else:
+            maximum = interval[1]
+
+    return len(intervals) - removed
 
 
 if __name__ == "__main__":
     pass
-    
