a96c8ceb2769da29b3dc73afd66d342802599e92
Allen1994 <chengcl1994@gmail.com>
2020-11-01 17:06:20 +0800

    20201101 complete

--- 20201101/get_decimal_value.py
+++ 20201101/get_decimal_value.py
@@ -1,6 +1,33 @@
+class ListNode:
+    def __init__(self, val=0, _next=None):
+        self.val = val
+        self.next = _next
+
+
+def head_list_generater(head_list):
+    pre_node_num = 0
+    pre_node = None
+    node_list = []
+    for val in head_list:
+        if pre_node_num != 0:
+            new_node = ListNode(val=val)
+            pre_node.next = new_node
+        else:
+            new_node = ListNode(val=val)
+        node_list.append(new_node)
+        pre_node = new_node
+        pre_node_num += 1
+    return node_list
+
+
 def get_decimal_value(head):
-	pass
-	
+    val = 0
+    while head.next != None:
+        val = val * 2 + head.val
+        head = head.next
+    val = val * 2 + head.val
+    return val
+
 
 if __name__ == "__main__":
     pass
--- 20201101/test_get_decimal_value.py
+++ 20201101/test_get_decimal_value.py
@@ -4,31 +4,40 @@ from get_decimal_value import *
 
 class GetDecimalValueTest(unittest.TestCase):
     def test_example_one(self):
-        head = [1,0,1]
+        head_input_list = [1,0,1]
+        head_list = head_list_generater(head_input_list)
+        head = head_list[0]
         expected = 5
         self.assertEqual(get_decimal_value(head), expected)
 
     def test_example_two(self):
-        head = [0]
+        head_input_list = [0]
+        head_list = head_list_generater(head_input_list)
+        head = head_list[0]
         expected = 0
         self.assertEqual(get_decimal_value(head), expected)
 
     def test_example_three(self):
-        head = [1]
+        head_input_list = [1]
+        head_list = head_list_generater(head_input_list)
+        head = head_list[0]
         expected = 1
         self.assertEqual(get_decimal_value(head), expected)
 
     def test_example_four(self):
-        head = [1,0,0,1,0,0,1,1,1,0,0,0,0,0,0]
+        head_input_list = [1,0,0,1,0,0,1,1,1,0,0,0,0,0,0]
+        head_list = head_list_generater(head_input_list)
+        head = head_list[0]
         expected = 18880
         self.assertEqual(get_decimal_value(head), expected)
 
-    def test_example_four(self):
-        head = [0,0]
+    def test_example_five(self):
+        head_input_list = [0,0]
+        head_list = head_list_generater(head_input_list)
+        head = head_list[0]
         expected = 0
         self.assertEqual(get_decimal_value(head), expected)
 
 
 if __name__ == "__main__":
     unittest.main()
-    
