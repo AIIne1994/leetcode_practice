c40196fd797eba5534090b40bfcf85e58288590c
Allen1994 <chengcl1994@gmail.com>
2020-11-07 21:17:55 +0800

    small_lest_divisor

--- 20201107/smallest_divisor.py
+++ 20201107/smallest_divisor.py
@@ -1,5 +1,16 @@
+import math
+
 def smallest_divisor(nums, threshold):
-    pass
+    compute_sum = lambda x : sum([math.ceil(n / x) for n in nums])
+    left, right = 1, nums[-1]
+    while left <= right:
+        pivot = (right + left) >> 1
+        num = compute_sum(pivot)
+        if num > threshold:
+            left = pivot + 1
+        else:
+            right = pivot - 1
+    return left
 
 
 if __name__ == "__main__":
--- 20201107/test_smallest_divisor.py
+++ 20201107/test_smallest_divisor.py
@@ -15,7 +15,7 @@ class SmallestDivisorTest(unittest.TestCase):
         expected = 3
         self.assertEqual(smallest_divisor(nums, threshold), expected)
 
-    def test_example_one(self):
+    def test_example_three(self):
         nums = [19]
         threshold = 5
         expected = 4
