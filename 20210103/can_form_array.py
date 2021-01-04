bf1651fd36e008e06beb18408f9d5e5f1efd2a48
allen_cheng <chengcl1994@gmail.com>
2021-01-04 15:51:30 +0800

    20210103_first_version

--- 20210103/can_form_array.py
+++ 20210103/can_form_array.py
@@ -1,7 +1,24 @@
+def connected_in_arr(arr, piece):
+    if piece[0] in arr:
+        head = arr.index(piece[0])
+        for i, num in enumerate(piece):
+            if (i+head) >= len(arr):
+                return False
+            if arr[i + head] != num:
+                return False
+        return True
+    else:
+        return False
+
+
 def canFormArray(arr, pieces):
     for piece in pieces:
-        pass
+        if connected_in_arr(arr, piece):
+            for num in piece:
+                arr.remove(num)
+
+    return arr == []
 
 
 if __name__ == '__main__':
-    pass
+    print(connected_in_arr([91, 4, 64, 78], [4, 64]))
