378067683c6cd44137ca1169e3f05a52095ce22a
chengcl1994 <chengcl1994@gamil.com>
2021-01-04 15:19:17 +0800

    20210103 init

---
+++ 20210103/can_form_array.py
@@ -0,0 +1,7 @@
+def canFormArray(arr, pieces):
+    for piece in pieces:
+        pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ 20210103/test_can_form_array.py
@@ -0,0 +1,38 @@
+from can_form_array import canFormArray
+import unittest
+
+
+class TestCanFormArray(unittest.TestCase):
+    def test_example_one(self):
+        arr = [85]
+        pieces = [[85]]
+        expected = True
+        self.assertEqual(canFormArray(arr, pieces), expected)
+
+    def test_example_two(self):
+        arr = [15, 88]
+        pieces = [[88], [15]]
+        expected = True
+        self.assertEqual(canFormArray(arr, pieces), expected)
+
+    def test_example_three(self):
+        arr = [49, 18, 16]
+        pieces = [[16, 18, 49]]
+        expected = False
+        self.assertEqual(canFormArray(arr, pieces), expected)
+
+    def test_example_four(self):
+        arr = [91, 4, 64, 78]
+        pieces = [[78], [4, 64], [91]]
+        expected = True
+        self.assertEqual(canFormArray(arr, pieces), expected)
+
+    def test_example_five(self):
+        arr = [1, 3, 5, 7]
+        pieces = [[2, 4, 6, 8]]
+        expected = False
+        self.assertEqual(canFormArray(arr, pieces), expected)
+        
+
+if __name__ == '__main__':
+    unittest.main()
